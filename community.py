682f0f2a51b21ff32f136fccc460dfb72fd2b5be
Hisen Zhang <993848217@qq.com>
2017-05-02 13:39:47 +0800

    Add files via upload

--- community.py
+++ community.py
@@ -26,6 +26,7 @@ import requests
 import re	
 import json
 from prettytable import PrettyTable as pt
+import statistic
 
 def com():
 	f = open('data.json','w+')
@@ -59,41 +60,10 @@ def com():
 	t.close
 	print 'All requests successfully recorded.'
 
-def sta():
-	with open('data.json','r+') as f:
-		x = f.readlines()
-	l = re.findall(r'["]([0-9]*[\.]*[0-9]+)%["]',str(x))
-	print len(l),'data is valid for statistic.\n'
-	counter_0 = 0
-	counter_1 = 0
-	counter_2 = 0
-	counter_3 = 0
-	counter_4 = 0
-	
-	for i in range(0,len(l)):
-		if float(l[i])<10:
-			counter_0 = counter_0 + 1
-		if float(l[i])>=10 and float(l[i])<25:
-			counter_1 = counter_1 + 1
-		if float(l[i])>=25 and float(l[i])<40:
-			counter_2 = counter_2 + 1
-		if float(l[i])>=40 and float(l[i])<55:
-			counter_3 = counter_3 + 1
-		if float(l[i])>55:
-			counter_4 = counter_4 + 1
-			
-	tb = pt(["RANGE","COUNT","PERCENTAGE",]) 	
-	tb.align["COUNT"] = "r" 
-	tb.add_row(["x<10",counter_0,float(counter_0/len(l)*100)])
-	tb.add_row(["10<=x<25",counter_1,float(counter_1/len(l)*100)])
-	tb.add_row(["25<=x<40",counter_2,float(counter_2/len(l)*100)])
-	tb.add_row(["40<=x<55",counter_3,float(counter_3/len(l)*100)])	
-	tb.add_row(["x>55",counter_4,float(counter_4/len(l)*100)])
-	print tb
 
 def main(args):
     com()
-    sta()
+    statistic.sta()
     return 0
 
 if __name__ == '__main__':
